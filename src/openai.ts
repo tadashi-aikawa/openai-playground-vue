a0c437f98934c2df80e65f829c2ed6828acda137
tadashi-aikawa <syou.maman@gmail.com>
2023-03-30 08:39:12 +0900

    Use chat completion API

--- src/openai.ts
+++ src/openai.ts
@@ -23,21 +23,30 @@ export async function question(input: string): Promise<Result> {
   });
   const openai = new OpenAIApi(configuration);
 
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `以下をJSONに変換してください。
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `結果のJSONだけを回答してください。解説や注意は不要です。
 
+以下をJSONに変換してください。
+
+----
 ${input}
+----
 
-以下はJSONの定義です。
+以下はJSONの定義です。他のプロパティは存在しません。
 
-- name (文字列)
+- name (会社名)
 - workSchedules (配列)
-    - employeeName
-    - beginTime (HH:mm)
-    - endTime (HH:mm)
+    - employeeName (従業員名)
+    - beginTime (例: 03:00)
+    - endTime (例: 19:00)
     - days (月火水木金土日からなる配列)
 `,
+      },
+    ],
     temperature: 0,
     max_tokens: 512,
     top_p: 1,
@@ -45,10 +54,9 @@ ${input}
     presence_penalty: 0,
   });
 
-  const yen = (response.data.usage!.total_tokens / 1000) * 0.02 * 132;
-  const jsonText = response.data.choices[0]
-    .text!.replace(/```json/, "")
-    .replace(/```/, "");
+  console.log(response.data);
+  const yen = (response.data.usage!.total_tokens / 1000) * 0.002 * 132;
+  const jsonText = response.data.choices[0].message?.content!;
 
   return {
     yen,
